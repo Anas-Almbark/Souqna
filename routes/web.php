4f2247fb80e48fb90d9acd08c794a8e57bd64442
Anas-Almbark0 <anas.almbark0@gmail.com>
2025-02-16 22:45:28 +0300

    init the project;

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::view("/", "shared.home")->name("home.index");
+Route::view("/home", "shared.home")->name("home.index");
+
+Route::view("/products", "shared.shopCategory")->name("products.index");
+Route::view("/product", "shared.shopSingle")->name("product.index");
+
+Route::view("/connect", "shared.connect")->name("connect.index");
+Route::view("/tracking", "shared.tracking")->name("tracking.index");
+
+
+require __DIR__ . '/auth.php';
