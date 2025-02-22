aab59ba0fcd649ea984fcf1172bb556f37fa55b9
karammaarouf <karamma3rouf@gmail.com>
2025-02-22 14:33:38 +0300

    added migration (admin) ,and update users (use migrate:fresh) to update database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('location')->nullable();
+            $table->string('identity')->nullable();
+            $table->string('photo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
