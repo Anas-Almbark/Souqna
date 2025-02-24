3e744ae88926d148fbde591ff364faea046e4793
karammaarouf <karamma3rouf@gmail.com>
2025-02-24 17:16:47 +0300

    side bar route added

--- resources/views/dashboardComponents/sideBar.blade.php
+++ resources/views/dashboardComponents/sideBar.blade.php
@@ -1,7 +1,7 @@
 <div class="nav-left-sidebar sidebar-dark">
     <div class="menu-list">
         <nav class="navbar navbar-expand-lg navbar-light">
-            <a class="d-xl-none d-lg-none" href="#">Dashboard</a>
+            <a class="d-xl-none d-lg-none" href="{{ route('dashboard') }}">Dashboard</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -14,7 +14,7 @@
                         <a class="nav-link" href="{{ route('dashboard') }}"><i class="fa fa-fw fa-user-circle"></i>Dashboard <span class="badge badge-success">6</span></a>
                     </li>
                     <li class="nav-item ">
-                        <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-user-circle"></i>Admins <span class="badge badge-success">6</span></a>
+                        <a class="nav-link" href="{{ route('admin.create') }}" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-user-circle"></i>Admins <span class="badge badge-success">6</span></a>
                         <div id="submenu-2" class="collapse submenu" style="">
                             <ul class="nav flex-column">
                                 <li class="nav-item">
