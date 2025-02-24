92c59c56c0c3af771c53ee5678c7816341f1ac0e
Anas-Almbark <coder.anas2@gmail.com>
2025-02-24 18:23:18 +0300

    handle message success

--- resources/views/dashboardComponents/admins.blade.php
+++ resources/views/dashboardComponents/admins.blade.php
@@ -1,6 +1,7 @@
 @extends("dashboard")
 @section("content")
 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+    @include("dashboardComponents.message")
     <div class="card">
       <div class="card-body">
         <div class="table-responsive">
