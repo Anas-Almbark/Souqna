d7abcaa9e0d2988276ae79381d85faec2ab3e9e3
karammaarouf <karamma3rouf@gmail.com>
2025-02-24 17:47:47 +0300

    added admin seeder

--- resources/views/dashboardComponents/sideBar.blade.php
+++ resources/views/dashboardComponents/sideBar.blade.php
@@ -2,7 +2,8 @@
     <div class="menu-list">
         <nav class="navbar navbar-expand-lg navbar-light">
             <a class="d-xl-none d-lg-none" href="{{ route('dashboard') }}">Dashboard</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
@@ -11,62 +12,26 @@
                         Menu
                     </li>
                     <li class="nav-item ">
-                        <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1"><i class="fa fa-fw fa-user-circle"></i>Dashboard <span class="badge badge-success">6</span></a>
-                        <div id="submenu-1" class="collapse submenu" style="">
-                            <ul class="nav flex-column">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1-2" aria-controls="submenu-1-2">E-Commerce</a>
-                                    <div id="submenu-1-2" class="collapse submenu" style="">
-                                        <ul class="nav flex-column">
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="{{route("dashboard")}}">E Commerce Dashboard</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="ecommerce-product.html">Product List</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="ecommerce-product-single.html">Product Single</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="ecommerce-product-checkout.html">Product Checkout</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="dashboard-finance.html">Finance</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="dashboard-sales.html">Sales</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1-1" aria-controls="submenu-1-1">Infulencer</a>
-                                    <div id="submenu-1-1" class="collapse submenu" style="">
-                                        <ul class="nav flex-column">
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="dashboard-influencer.html">Influencer</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="influencer-finder.html">Influencer Finder</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link" href="influencer-profile.html">Influencer Profile</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
+                        <a class="nav-link active" href="{{ route('dashboard') }}"><i
+                                class="fa fa-fw fa-user-circle"></i>Dashboard <span
+                                class="badge badge-success">6</span></a>
                     </li>
                     <li class="nav-item ">
-                        <a class="nav-link" href="{{ route('admin.create') }}" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-user-circle"></i>Admins <span class="badge badge-success">6</span></a>
+                        <a class="nav-link" href="{{ route('admin.create') }}" data-toggle="collapse"
+                            aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i
+                                class="fa fa-fw fa-user-circle"></i>Admins <span
+                                class="badge badge-success">6</span></a>
                         <div id="submenu-2" class="collapse submenu" style="">
                             <ul class="nav flex-column">
-                            <li class="nav-item">
-                            <a class="nav-link" href="{{route("admin.index")}}"><i class="fa fa-fw fa-user-circle"></i>Show Admins<span class="badge badge-success">6</span></a>
-                            <a class="nav-link" href="{{route("admin.create")}}"><i class="fa fa-fw fa-user-circle"></i>Add Admin<span class="badge badge-success">6</span></a>
-                                    </li>
-                                </ul>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('admin.index') }}"><i
+                                            class="fa fa-fw fa-user-circle"></i>Show Admins<span
+                                            class="badge badge-success">6</span></a>
+                                    <a class="nav-link" href="{{ route('admin.create') }}"><i
+                                            class="fa fa-fw fa-user-circle"></i>Add Admin<span
+                                            class="badge badge-success">6</span></a>
+                                </li>
+                            </ul>
                         </div>
                     </li>
                 </ul>
