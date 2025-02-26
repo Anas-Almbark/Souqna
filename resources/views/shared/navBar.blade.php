ae37e4654d3542f6bb63cbe76b6ee726e27c6c7d
hasan <hasnshhade90@gmail.com>
2025-02-26 20:51:04 +0300

    revision

--- resources/views/shared/navBar.blade.php
+++ resources/views/shared/navBar.blade.php
@@ -1,6 +1,5 @@
 <header class="header_area">
     <div class="main_menu">
-<<<<<<< HEAD
       <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
           <a class="navbar-brand logo_h" href="{{ route("home.index") }}"><img src="img/logo.png" alt=""></a>
@@ -78,74 +77,5 @@
           </div>
         </div>
       </nav>
-=======
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="container">
-                <a class="navbar-brand logo_h" href="{{ route('home.index') }}"><img src="{{ asset('img/logo.png') }}"
-                        alt=""></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-                    <ul class="nav navbar-nav menu_nav ml-auto mr-auto">
-                        <li class="nav-item active"><a class="nav-link" href="{{ route('home.index') }}">Home</a></li>
-                        <li class="nav-item submenu dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
-                                aria-haspopup="true" aria-expanded="false">Shop</a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item"><a class="nav-link" href="{{ route('products.index') }}">Shop
-                                        Category</a></li>
-                                <li class="nav-item"><a class="nav-link" href="{{ route('product.index') }}">Product
-                                        Details</a></li>
-                                <li class="nav-item"><a class="nav-link" href="checkout.html">Product Checkout</a></li>
-                                <li class="nav-item"><a class="nav-link" href="confirmation.html">Confirmation</a></li>
-                                <li class="nav-item"><a class="nav-link" href="cart.html">Shopping Cart</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item submenu dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
-                                aria-haspopup="true" aria-expanded="false">Blog</a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
-                                <li class="nav-item"><a class="nav-link" href="single-blog.html">Blog Details</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item submenu dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
-                                aria-haspopup="true" aria-expanded="false">Pages</a>
-                            <ul class="dropdown-menu">
-                                @if (!auth()->user())
-                                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                                    <li class="nav-item"><a class="nav-link" href={{ route('register') }}>Register</a>
-                                    </li>
-                                @endif
-                                <li class="nav-item"><a class="nav-link"
-                                        href="{{ route('tracking.index') }}">Tracking</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="{{ route('connect.index') }}">Contact</a></li>
-                    </ul>
-                    <ul class="nav-shop flex items-center">
-                        @if (auth()->user())
-                            <li class="nav-item"><a class="mx-3" href="{{ route('profile.index') }}">
-                                    <img src="{{ asset('img/def.png') }}" alt="Profile" class="rounded-circle"
-                                        width="40" height="40">
-                                </a></li>
-                            <li class="nav-item">
-                                <form action="{{ route('logout') }}" method="post">
-                                    @csrf
-                                    <button type="submit" class="button button-header border py-2 px-3"
-                                        href="{{ route('logout') }}">Logout</button>
-                                </form>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
->>>>>>> 9436a70542232e97f874a973ea378788fda6b2c3
     </div>
-</header>
+  </header>
