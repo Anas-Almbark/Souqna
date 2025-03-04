0e8a5269da9a8f5248b9dec29a4a32b40f7f6b15
hasan <hasnshhade90@gmail.com>
2025-03-04 04:05:09 +0300

    update products in dashb

--- resources/views/adminproducts/products.blade.php
+++ resources/views/adminproducts/products.blade.php
@@ -1,41 +1,55 @@
 @extends('dashboard')
 @section('content')
-    <section class="section-margin">
-        <div class="container">
-            <div class="row">
-                @foreach ($products as $product)
-                    <div class="col-md-6 col-lg-4 col-xl-3">
-                        <div class="card text-center card-product">
-                            <div class="card-product__img">
-                                <img class="card-img" src="{{ Storage::url($product->photos->first()->url) }}"
-                                    alt="{{ $product->name }}">
-                            </div>
-                            <div class="card-body">
-                                <h4 class="card-product__title"><a
-                                        href="{{ route('products.show', $product->id) }}">{{ $product->name }}</a></h4>
-                                <p class="card-product__price">${{ $product->price }}</p>
-                                <p class="card-product__description">{{ $product->description }}</p>
-                                <div class="admin-actions">
-                                    <form action="{{ route('admin.products.approve', $product->id) }}" method="POST"
-                                        class="d-inline">
-                                        @csrf
-                                        <button type="submit" class="btn btn-success">Approval</button>
-                                    </form>
-                                    <form action="{{ route('admin.products.reject', $product->id) }}" method="POST"
-                                        class="d-inline">
-                                        @csrf
-                                        <button type="submit" class="btn btn-danger">rejection</button>
-                                    </form>
-                                    <a href="{{ route('products.show', $product->id) }}"
-                                        class="btn btn-info text-white rounded-pill px-4 rounded mt-3">
-                                        <i class="fa fa-eye"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
+<section class="section-margin">
+    <div class="container">
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>Status</th>
+                        <th>User</th>
+                        <th>Created At</th>
+                        <th>Updated At</th>
+                        <th>Actions</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                        <tr>
+                            <td>{{ $product->id }}</td>
+                            <td>
+                                <a href="{{ route('products.show', $product->id) }}">{{ $product->name }}</a>
+                            </td>
+                            <td>{{ $product->description }}</td>
+                            <td>${{ $product->price }}</td>
+                            <td>{{ $product->status }}</td>
+                            <td>{{ optional($product->user)->name ?? $product->user_id }}</td>
+                            <td>{{ $product->created_at->format('Y-m-d H:i') }}</td>
+                            <td>{{ $product->updated_at->format('Y-m-d H:i') }}</td>
+                            <td>
+                                <form action="{{ route('admin.products.approve', $product->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    <button type="submit" class="btn btn-success btn-sm">Approval</button>
+                                </form>
+                                <form action="{{ route('admin.products.reject', $product->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    <button type="submit" class="btn btn-danger btn-sm">Rejection</button>
+                                </form>
+                                <a href="{{ route('products.show', $product->id) }}" class="btn btn-info text-white rounded-pill px-4 rounded mt-3 btn-sm">
+                                    <i class="fa fa-eye"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
-    </section>
+    </div>
+</section>
+
 @endsection
